83d2c6ec0d9ae3836710031cb3d339ecc190b047
blackknight9113-star <blackknight9113@gmail.com>
2026-03-10 00:53:12 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,18 @@
+<meta name='viewport' content='width=device-width, initial-scale=1'/><script>const questionBank = [
+    {
+        q: "The average weight of 40 students is 52 kg. If the teacher's weight is included, the average rises by 500g. Find teacher's weight.",
+        o: ["72.5 kg", "74.5 kg", "73.5 kg", "71.5 kg"],
+        a: 0,
+        sub: "Maths",
+        topic: "Average",
+        sol: "41 × 52.5 - 40 × 52 = 72.5 kg"
+    },
+    {
+        q: "What is the average of first 10 even natural numbers?",
+        o: ["10", "11", "12", "15"],
+        a: 1,
+        sub: "Maths",
+        topic: "Series",
+        sol: "Formula: n + 1 = 10 + 1 = 11"
+    }
+];</script>
