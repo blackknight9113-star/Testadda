ed356e70d9f6123ed736834b87ba3afc061c2050
blackknight9113-star <blackknight9113@gmail.com>
2026-03-10 01:50:29 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-<meta name='viewport' content='width=device-width, initial-scale=1'/><script>const questionBank = [
+const questionBank = [
     {
         q: "The average weight of 40 students is 52 kg. If the teacher's weight is included, the average rises by 500g. Find teacher's weight.",
         o: ["72.5 kg", "74.5 kg", "73.5 kg", "71.5 kg"],
@@ -15,4 +15,4 @@
         topic: "Series",
         sol: "Formula: n + 1 = 10 + 1 = 11"
     }
-];</script>
+];
