84336fb52c8537875457399f021d4ea972f87109
blackknight9113-star <blackknight9113@gmail.com>
2026-03-12 15:27:04 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -3,7 +3,7 @@ const questionBank = [
         q: "The average weight of 40 students is 52 kg. If the teacher's weight is included, the average rises by 500g. Find teacher's weight.",
         o: ["72.5 kg", "74.5 kg", "73.5 kg", "71.5 kg"],
         a: 0,
-        sub: "Maths",
+        s: "Mathematics",
         t: "Average",
         sol: "41 × 52.5 - 40 × 52 = 72.5 kg",
         
@@ -12,7 +12,7 @@ const questionBank = [
         q: "What is the average of first 10 even natural numbers?",
         o: ["10", "11", "12", "15"],
         a: 1,
-        sub: "Maths",
+        s: "Mathematics",
         t: "Series",
         sol: "Formula: n + 1 = 10 + 1 = 11"
     }
